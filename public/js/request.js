51115a6b61fa565cd3b6ea0d8b1479157c868ce7
Tlholo <58808719+tlholoMakhene-afk@users.noreply.github.com>
2020-09-26 14:00:45 +0200

    Add files via upload

---
+++ public/js/request.js
@@ -0,0 +1,14 @@
+const key = '098f0da2bcb8559b8104af813e3b839e';
+
+const requestCity = async (city) => {
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
